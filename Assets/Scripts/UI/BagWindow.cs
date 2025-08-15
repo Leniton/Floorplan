ac9e7c337b4525934844d9dd404ee33b54c93e5a
Leniton <lenitoncarneiro@gmail.com>
2025-08-15 15:21:09 -0300

    feat: added bag window references

--- Assets/Scripts/UI/BagWindow.cs
+++ Assets/Scripts/UI/BagWindow.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BagWindow : MonoBehaviour
 {
+    [SerializeField] private Toggle autoPickupToggle;
+    [SerializeField] private Button openDetailsButton;
     [SerializeField] private FloorplanDetails floorplanDetails;
     [SerializeField] private ItemButton itemPrefab;
 
