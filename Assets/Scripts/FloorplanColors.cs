584f428e344b4655aa25db770f6f3398a6e9238f
Leniton <lenitoncarneiro@gmail.com>
2025-08-23 15:58:51 -0300

    feat: floorplan colors scriptable

---
+++ Assets/Scripts/FloorplanColors.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Floorplan", menuName = "Floorplan/Floorplan Colors")]
+public class FloorplanColors : ScriptableObject
+{
+    public Color blankColor;
+    public Color restRoomColor;
+    public Color hallwayColor;
+    public Color storageRoomColor;
+    public Color fancyRoomColor;
+    public Color shopColor;
+    public Color mysteryRoomColor;
+    public Color cursedRoomColor;
+}
